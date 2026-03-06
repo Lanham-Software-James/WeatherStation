d990e00603aef9319ed68e6f126e0796393eb825
James Lanham <james.r.lanham2021@gmail.com>
2026-03-06 13:26:51 -0500

    Adding sensor base class template with unit tests

---
+++ test/mocks/MockSensor.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <string>
+
+#include "sensors/Sensor.h"
+
+class FakeSensor : public Sensor
+{
+public:
+    bool initialize_result{true};
+    bool read_result{true};
+
+    bool on_initialize_called{false};
+    bool on_read_called{false};
+
+    float temperature_to_write{22.5f};
+    float humidity_to_write{50.0f};
+    float pressure_to_write{1013.25f};
+
+    [[nodiscard]] std::string getName() const override
+    {
+        return "FakeSensor";
+    }
+
+protected:
+    bool onInitialize() override
+    {
+        on_initialize_called = true;
+        return initialize_result;
+    }
+
+    bool onRead(Observation& observation) override
+    {
+        on_read_called = true;
+
+        if (!read_result)
+        {
+            return false;
+        }
+
+        observation.temperature_c = temperature_to_write;
+        observation.humidity_pct = humidity_to_write;
+        observation.pressure_hpa = pressure_to_write;
+        return true;
+    }
+};
